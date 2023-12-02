84b7c9aa5febfc84640c7a68c48ab7438729226d
digpolo <digpolo@gmail.com>
2023-12-02 10:58:41 -0500

    add structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -6,8 +7,10 @@ const Navbar = () => {
 
   return (
     <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
+      <Link to='/'><h4>Home</h4></Link>
+      <Link to='/contact'><h4>Contact</h4></Link>
+      <Link to='/details'><h4>details</h4></Link>
+      <Link to='/favs'><h4>Favs</h4></Link>
       <button>Change theme</button>
     </nav>
   )
